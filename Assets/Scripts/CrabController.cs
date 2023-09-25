f69b6ddb416df71328bbce36c47cdf459d2139f5
Alex M <almaynard2404@gmail.com>
2023-09-24 23:15:13 -0500

    Added more assets and state state machine
    Added tilemap texture, clouds and grass to the project. Also finised state machine for the crab and implemented patrol on the CrabController

---
+++ Assets/Scripts/CrabController.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CrabController : MonoBehaviour
+{
+    //player variables
+    public GameObject player;
+
+
+    //crab variables
+    public float health;
+    public float speed = 6;
+    private Vector3 nextPosition;
+    public Transform[] positions = new Transform[4]; //startPosition, position1, position2, position3, position4;
+
+
+    //crab states declared
+    public StateMachine stateMachine;
+    public StateMachine.State patrol, chase, attack, runAway, damage; //maybe add death state
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //new instance of StateMachine
+        stateMachine = new StateMachine();
+
+
+        //sets first waypoint on start()
+        nextPosition = positions[0].position;
+
+
+        //Use factory pattern
+        //patrol state
+        patrol = stateMachine.CreateState("Patrol");
+        patrol.onEnter = delegate { Debug.Log("Patrol.onEnter"); };
+        patrol.onExit = delegate { Debug.Log("Patrol.onExit"); };
+        patrol.onFrame = PatrolOnFrame;
+
+
+        //chase state
+        chase = stateMachine.CreateState("Chase");
+        chase.onEnter = delegate { Debug.Log("Chase.onEnter"); };
+        chase.onExit = delegate { Debug.Log("Chase.onExit"); };
+        chase.onFrame = ChaseOnFrame;
+
+
+        //attack state
+        attack = stateMachine.CreateState("Attack");
+        attack.onEnter = delegate { Debug.Log("Attack.onEnter"); };
+        attack.onExit = delegate { Debug.Log("Attack.onExit"); };
+        attack.onFrame = AttackOnFrame;
+
+
+        //runAway state
+        runAway = stateMachine.CreateState("RunAway");
+        runAway.onEnter = delegate { Debug.Log("RunAway.onEnter"); };
+        runAway.onExit = delegate { Debug.Log("RunAway.onExit"); };
+        runAway.onFrame = RunAwayOnFrame;
+
+
+        //damage state
+        damage = stateMachine.CreateState("Damage");
+        damage.onEnter = delegate { Debug.Log("Damage.onEnter"); };
+        damage.onExit = delegate { Debug.Log("Damage.onExit"); };
+        damage.onFrame = DamageOnFrame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Update();
+    }
+
+
+    void PatrolOnFrame()
+    {
+        //decides which point the platform should move to at any given time
+        if (transform.position == positions[0].position)
+        {
+            nextPosition = positions[1].position;
+        } else if (transform.position == positions[1].position)
+        {
+            nextPosition = positions[2].position;
+        } else if (transform.position == positions[2].position)
+        {
+            nextPosition = positions[3].position;
+        } else if (transform.position == positions[3].position)
+        {
+            nextPosition = positions[0].position;
+        }
+
+        //moves the platform
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, (speed * Time.deltaTime));
+    }
+
+
+    void ChaseOnFrame()
+    {
+
+    }
+
+
+    void AttackOnFrame()
+    {
+
+    }
+
+
+    void RunAwayOnFrame()
+    {
+
+    }
+
+
+    void DamageOnFrame()
+    {
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(positions[0].position, positions[1].position);
+        Gizmos.DrawLine(positions[1].position, positions[2].position);
+        Gizmos.DrawLine(positions[2].position, positions[3].position);
+        Gizmos.DrawLine(positions[3].position, positions[0].position);
+    }
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    public class State
+    {
+        public string Name;
+        public System.Action onFrame; //Default action to be performed (when the state is active)
+        public System.Action onEnter; //When state is entered
+        public System.Action onExit; //When state is exited from 
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public Dictionary<string, State> states = new Dictionary<string, State>();
+    public State currentState { get; private set; }
+    public State initialState;
+
+    public State CreateState(string name)
+    {
+        State state = new State();
+        state.Name = name;
+
+        if(states.Count == 0)
+        {
+            initialState = state;
+        }
+
+        states[name] = state;
+
+        return state;
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //No states yet
+        if (states.Count == 0)
+        {
+            Debug.LogError("*** State machine has no states! ***");
+            return;
+        }
+
+        //no current state yet
+        if (currentState == null)
+        {
+            ChangeState(initialState);
+        }
+
+        if (currentState.onFrame != null)
+        {
+            currentState.onFrame();
+        }
+    }
+
+
+    public void ChangeState(State newState)
+    {
+        //
+        if (newState == null)
+        {
+            Debug.LogError("*** Can't change to a null state! ***");
+            return;
+        }
+        //do onExit of current state
+        if (currentState != null && currentState.onExit != null)
+        {
+            currentState.onExit();
+        }
+
+
+        //change to newState
+        Debug.LogFormat("*** Changing from state {0} to state {1} ***", currentState, newState);
+        currentState = newState;
+
+
+
+        // do onEnter to the newState
+        if (currentState.onEnter != null)
+        {
+            currentState.onEnter();
+        }
+    }
+
+    public void ChangeState(string newStateName)
+    {
+        if (states.ContainsKey(newStateName))
+        {
+            ChangeState(states[newStateName]);
+        }
+        else
+        {
+            Debug.LogErrorFormat("*** State machine doesn't have the state {0} ***", newStateName);
+            return;
+        }
+
+    }
+}
